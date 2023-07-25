2a83abee2123292164f7e916598113855ef2bc18
mayanhavoc <mayanhavoc@gmail.com>
2023-07-25 16:49:33 -0400

    change secure to false

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ const password = process.env.GMAIL_PASSWORD;
 const transporter = nodemailer.createTransport({
 	host: 'smtp.forwardemail.net',
 	port: 465,
-	secure: true,
+	secure: false,
 	auth: {
 		user: 'realficardano@gmail.com',
 		pass: password,
