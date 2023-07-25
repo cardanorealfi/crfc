cdfe0537c6b205e8de63e02aef7a750d3066145f
mayanhavoc <mayanhavoc@gmail.com>
2023-07-25 16:58:27 -0400

    set gmail as nodemailer service

--- app.js
+++ app.js
@@ -144,8 +144,7 @@ passport.deserializeUser(User.deserializeUser());
 const password = process.env.GMAIL_PASSWORD;
 
 const transporter = nodemailer.createTransport({
-	host: 'smtp.forwardemail.net',
-	port: 465,
+	service: 'gmail',
 	secure: true,
 	auth: {
 		user: 'realficardano@gmail.com',
